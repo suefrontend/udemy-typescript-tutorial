6154f1ecde5bb0ec5126f390b734145a0ae4201f
Kayano Oyama <kayano@shearcomfort.com>
2024-04-01 21:55:52 -0700

    Add enum:

---
+++ section-11-csv-analyzer/build/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+let manUnitedWins = 0;
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log('Won: ', manUnitedWins);
---
+++ section-11-csv-analyzer/src/index.ts
@@ -0,0 +1,22 @@
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+let manUnitedWins = 0;
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log('Won: ', manUnitedWins);
